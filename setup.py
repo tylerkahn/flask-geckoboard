5df4610d5e94a399d60823024b0b4d24401a02e4
Rafael <rafael@resmio.com>
2015-08-19 17:41:05 +0200

    Pep8.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, Command
 import os
+from setuptools import setup
 
 import flask_geckoboard
 
@@ -7,33 +7,34 @@ import flask_geckoboard
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 def build_long_description():
     return "\n\n".join([
-        flask_geckoboard.__doc__,  #@UndefinedVariable
+        flask_geckoboard.__doc__,  # @UndefinedVariable
         read('CHANGELOG.rst'),
     ])
 
 
 setup(
-    name = 'Flask-Geckoboard',
-    version = flask_geckoboard.__version__,
-    license = flask_geckoboard.__license__,
-    description = 'Geckoboard custom widgets for Flask projects',
-    long_description = build_long_description(),
-    author = flask_geckoboard.__author__,
-    author_email = flask_geckoboard.__email__,
-    packages = [
+    name='Flask-Geckoboard',
+    version=flask_geckoboard.__version__,
+    license=flask_geckoboard.__license__,
+    description='Geckoboard custom widgets for Flask projects',
+    long_description=build_long_description(),
+    author=flask_geckoboard.__author__,
+    author_email=flask_geckoboard.__email__,
+    packages=[
         'flask_geckoboard',
         'tests',
     ],
-    install_requires = (
+    install_requires=(
         'flask'
     ),
-    extras_require = {
+    extras_require={
         'encryption': ['pycrypto']
     },
-    keywords = ['flask', 'geckoboard'],
-    classifiers = [
+    keywords=['flask', 'geckoboard'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Flask',
@@ -44,7 +45,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    platforms = ['any'],
-    url = 'http://github.com/PolymathVentures/flask-geckoboard',
-    download_url = 'http://github.com/PolymathVentures/flask-geckoboard/archives/master',
+    platforms=['any'],
+    url='http://github.com/neocortex/flask-geckoboard',
+    download_url='http://github.com/neocortex/flask-geckoboard/archives/master',
 )
