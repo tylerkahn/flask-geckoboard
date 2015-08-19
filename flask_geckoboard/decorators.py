640691c1fa79dfc136e8e9b453e50db9662aeff9
Rafael <rafael@resmio.com>
2015-08-19 17:34:32 +0200

    Minor changes, pep8 fixes.

--- flask_geckoboard/decorators.py
+++ flask_geckoboard/decorators.py
@@ -1,6 +1,4 @@
-"""
-Geckoboard decorators.
-"""
+""" Geckoboard decorators. """
 
 import base64
 import json
@@ -33,15 +31,16 @@ class WidgetDecorator(object):
     Geckoboard widget decorator.
 
     The decorated view must return a data structure suitable for
-    serialization to XML or JSON for Geckoboard.  See the Geckoboard
+    serialization to XML or JSON for Geckoboard. See the Geckoboard
     API docs or the source of extending classes for details.
 
     If the ``GECKOBOARD_API_KEY`` setting is used, the request must
     contain the correct API key, or a 403 Forbidden response is
     returned.
 
-    If the ``encrypted` argument is set to True, then the data will be
+    If the ``encrypted`` argument is set to True, then the data will be
     encrypted using ``GECKOBOARD_PASSWORD`` (JSON only).
+
     """
     def __new__(cls, *args, **kwargs):
         obj = object.__new__(cls)
@@ -49,8 +48,8 @@ class WidgetDecorator(object):
         if 'encrypted' in kwargs:
             if not encryption_enabled:
                 raise GeckoboardException(
-                    'Use of encryption requires the pycrypto package. ' + \
-                    'This package can be installed manually or by enabling ' + \
+                    'Use of encryption requires the pycrypto package. ' +
+                    'This package can be installed manually or by enabling ' +
                     'the encryption feature during installation.'
                 )
             obj._encrypted = kwargs.pop('encrypted')
@@ -74,7 +73,8 @@ class WidgetDecorator(object):
                 self.data.update(data)
             except ValueError:
                 self.data = data
-            content, content_type = _render(self.data, self._encrypted, self._format)
+            content, content_type = _render(
+                self.data, self._encrypted, self._format)
             return app.response_class(content, mimetype=content_type)
         return decorated_view
 
@@ -91,9 +91,9 @@ class NumberWidgetDecorator(WidgetDecorator):
 
     The decorated view must return a tuple `(current, [previous])`, where
     `current` is the current value and `previous` is the previous value
-    of the measured quantity..
-    """
+    of the measured quantity.
 
+    """
     def _convert_view_result(self, result):
         if not isinstance(result, (tuple, list)):
             result = [result]
@@ -110,12 +110,12 @@ class RAGWidgetDecorator(WidgetDecorator):
     Geckoboard Red-Amber-Green (RAG) widget decorator.
 
     The decorated view must return a tuple with three tuples `(value,
-    [text])`.  The `value` parameters are the numbers shown in red,
+    [text])`. The `value` parameters are the numbers shown in red,
     amber and green (in that order).  The `text` parameters are optional
     and will be displayed next to the respective values in the
     dashboard.
-    """
 
+    """
     def _convert_view_result(self, result):
         items = []
         for elem in result:
@@ -140,12 +140,12 @@ class TextWidgetDecorator(WidgetDecorator):
 
     The decorated view must return a list of tuples `(message, [type])`.
     The `message` parameters are strings that will be shown in the
-    widget.  The `type` parameters are optional and tell Geckoboard how
-    to annotate the messages.  Use ``TEXT_INFO`` for informational
+    widget. The `type` parameters are optional and tell Geckoboard how
+    to annotate the messages. Use ``TEXT_INFO`` for informational
     messages, ``TEXT_WARN`` for for warnings and ``TEXT_NONE`` for plain
     text (the default).
-    """
 
+    """
     def _convert_view_result(self, result):
         items = []
         if not isinstance(result, (tuple, list)):
@@ -170,10 +170,10 @@ class PieChartWidgetDecorator(WidgetDecorator):
     Geckoboard Pie chart decorator.
 
     The decorated view must return a list of tuples `(value, label,
-    color)`.  The color parameter is a string 'RRGGBB[TT]' representing
+    color)`. The color parameter is a string 'RRGGBB[TT]' representing
     red, green, blue and optionally transparency.
-    """
 
+    """
     def _convert_view_result(self, result):
         items = []
         for elem in result:
@@ -196,15 +196,15 @@ class LineChartLegacyWidgetDecorator(WidgetDecorator):
     Geckoboard Line chart (legacy) decorator.
 
     The decorated view must return a tuple `(values, x_axis, y_axis,
-    [color])`.  The `values` parameter is a list of data points.  The
+    [color])`. The `values` parameter is a list of data points. The
     `x-axis` parameter is a label string or a list of strings, that will
-    be placed on the X-axis.  The `y-axis` parameter works similarly for
-    the Y-axis.  If there are more than one axis label, they are placed
-    evenly along the axis.  The optional `color` parameter is a string
+    be placed on the X-axis. The `y-axis` parameter works similarly for
+    the Y-axis. If there are more than one axis label, they are placed
+    evenly along the axis. The optional `color` parameter is a string
     ``'RRGGBB[TT]'`` representing red, green, blue and optionally
     transparency.
-    """
 
+    """
     def _convert_view_result(self, result):
         data = OrderedDict()
         data['item'] = list(result[0])
@@ -254,10 +254,10 @@ class LineChartWidgetDecorator(WidgetDecorator):
     def _convert_view_result(self, result):
         data = OrderedDict()
         if not isinstance(result['series'], list) or 'series' not in result:
-            raise RuntimeError, 'Key "series" (list) is required'
+            raise RuntimeError('Key "series" (list) is required')
         for s in result.get('series'):
             if not isinstance(s, dict) or 'data' not in s:
-                raise RuntimeError, 'Series must contain "data" entry'
+                raise RuntimeError('Series must contain "data" entry')
         data['series'] = result.get('series')
 
         if 'x_axis' in result:
@@ -275,14 +275,14 @@ class GeckOMeterWidgetDecorator(WidgetDecorator):
     """
     Geckoboard Geck-O-Meter decorator.
 
-    The decorated view must return a tuple `(value, min, max)`.  The
-    `value` parameter represents the current value.  The `min` and `max`
+    The decorated view must return a tuple `(value, min, max)`. The
+    `value` parameter represents the current value. The `min` and `max`
     parameters represent the minimum and maximum value respectively.
-    They are either a value, or a tuple `(value, text)`.  If used, the
+    They are either a value, or a tuple `(value, text)`. If used, the
     `text` parameter will be displayed next to the minimum or maximum
     value.
-    """
 
+    """
     def _convert_view_result(self, result):
         value, min, max = result
         data = OrderedDict()
@@ -322,8 +322,8 @@ class FunnelWidgetDecorator(WidgetDecorator):
                     not the percentage value is shown.
         sort:       `False` (default) or `True`. Sort the entries by
                     value or not.
-    """
 
+    """
     def _convert_view_result(self, result):
         data = OrderedDict()
         items = result.get('items', [])
@@ -332,9 +332,9 @@ class FunnelWidgetDecorator(WidgetDecorator):
         if result.get('sort'):
             items.sort(reverse=True)
 
-        data["item"] = [dict(zip(("value","label"), item)) for item in items]
-        data["type"] = result.get('type', 'standard')
-        data["percentage"] = result.get('percentage','show')
+        data['item'] = [dict(zip(('value', 'label'), item)) for item in items]
+        data['type'] = result.get('type', 'standard')
+        data['percentage'] = result.get('percentage', 'show')
         return data
 
 funnel = FunnelWidgetDecorator
@@ -342,9 +342,6 @@ funnel = FunnelWidgetDecorator
 
 class BulletWidgetDecorator(WidgetDecorator):
     """
-    See http://support.geckoboard.com/entries/274940-custom-chart-widget-type-definitions
-    for more information.
-
     The decorated method must return a dictionary containing these keys:
 
     Required keys:
@@ -369,8 +366,8 @@ class BulletWidgetDecorator(WidgetDecorator):
                     do not fit into Geckoboard's UI, eg. a value of 1100
                     is represented as 1.1. If scaling takes place the sublabel
                     is suffixed with that information. Default is true.
-    """
 
+    """
     def _convert_view_result(self, results):
         # Check required keys. We do not do type checking since this level of
         # competence is assumed.
@@ -379,16 +376,16 @@ class BulletWidgetDecorator(WidgetDecorator):
         items = []
         for result in results:
             for key in ('label', 'axis_points', 'current'):
-                if not result.has_key(key):
-                    raise RuntimeError, "Key %s is required" % key
+                if key not in result:
+                    raise RuntimeError('Key {} is required'.format(key))
 
             # Handle singleton current and projected
             current = result['current']
             projected = result.get('projected', None)
             if not isinstance(current, (ListType, TupleType)):
                 current = [0, current]
-            if (projected is not None) and not isinstance(projected, (ListType,
-                    TupleType)):
+            if (projected is not None) and not isinstance(
+                    projected, (ListType, TupleType)):
                 projected = [0, projected]
 
             # If red, amber and green are not *all* supplied calculate defaults
@@ -418,7 +415,7 @@ class BulletWidgetDecorator(WidgetDecorator):
             auto_scale = result.get('auto_scale', True)
             if auto_scale and axis_points:
                 scale_label_map = {1000000000: 'billions', 1000000: 'millions',
-                        1000: 'thousands'}
+                                   1000: 'thousands'}
                 scale = 1
                 value = max(axis_points)
                 for n in (1000000000, 1000000, 1000):
@@ -434,21 +431,23 @@ class BulletWidgetDecorator(WidgetDecorator):
                 # Apply scale to all values
                 if scale > 1:
                     axis_points = [scaler(v, scale) for v in axis_points]
-                    current = (scaler(current[0], scale), scaler(current[1], scale))
+                    current = (scaler(current[0], scale),
+                               scaler(current[1], scale))
                     if projected is not None:
                         projected = (scaler(projected[0], scale),
-                                scaler(projected[1], scale))
-                    red = (scaler(red[0], scale), scaler(red[1], scale))
-                    amber = (scaler(amber[0], scale), scaler(amber[1], scale))
-                    green = (scaler(green[0], scale), scaler(green[1], scale))
+                                     scaler(projected[1], scale))
+                    # red = (scaler(red[0], scale), scaler(red[1], scale))
+                    # amber = (scaler(amber[0], scale), scaler(amber[1], scale))
+                    # green = (scaler(green[0], scale), scaler(green[1], scale))
                     if 'comparative' in result:
-                        result['comparative'] = scaler(result['comparative'], scale)
+                        result['comparative'] = scaler(
+                            result['comparative'], scale)
 
                     # Suffix sublabel
                     sublabel = result.get('sublabel', '')
                     if sublabel:
-                        result['sublabel'] = '%s (%s)' % \
-                                (sublabel, scale_label_map[scale])
+                        result['sublabel'] = '{} ({})'.format(
+                            sublabel, scale_label_map[scale])
                     else:
                         result['sublabel'] = scale_label_map[scale].capitalize()
 
@@ -462,11 +461,11 @@ class BulletWidgetDecorator(WidgetDecorator):
                 data['comparative'] = dict(point=result['comparative'])
 
             # Add optional items
-            if result.has_key('sublabel'):
+            if 'sublabel' in result:
                 data['sublabel'] = result['sublabel']
             if projected is not None:
                 data['measure']['projected'] = dict(start=projected[0],
-                        end=projected[1])
+                                                    end=projected[1])
 
             items.append(data)
         return dict(item=items,
@@ -493,7 +492,6 @@ class LeaderboardWidgetDecorator(WidgetDecorator):
         (labels, values)
 
     """
-
     def _convert_view_result(self, result):
         data = OrderedDict()
         sort_arg = 'descending'
@@ -518,7 +516,7 @@ leaderboard = LeaderboardWidgetDecorator
 
 
 def _is_api_key_correct():
-    """Return whether the Geckoboard API key on the request is correct."""
+    """ Return whether the Geckoboard API key on the request is correct. """
     api_key = app.config.get('GECKOBOARD_API_KEY')
     if api_key is None:
         return True
@@ -528,6 +526,7 @@ def _is_api_key_correct():
             return auth.username == api_key and auth.password == 'X'
     return False
 
+
 def _derive_key_and_iv(password, salt, key_length, iv_length):
     d = d_i = ''
     while len(d) < key_length + iv_length:
@@ -535,8 +534,9 @@ def _derive_key_and_iv(password, salt, key_length, iv_length):
         d += d_i
     return d[:key_length], d[key_length:key_length+iv_length]
 
+
 def _encrypt(data):
-    """Equivalent to OpenSSL using 256 bit AES in CBC mode"""
+    """ Equivalent to OpenSSL using 256 bit AES in CBC mode. """
     BS = AES.block_size
     pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
     password = app.config.get('GECKOBOARD_PASSWORD')
@@ -546,6 +546,7 @@ def _encrypt(data):
     encrypted = 'Salted__' + salt + cipher.encrypt(pad(data))
     return base64.b64encode(encrypted)
 
+
 def _render(data, encrypted, format=None):
     """
     Render the data to Geckoboard. If the `format` parameter is passed
@@ -554,8 +555,10 @@ def _render(data, encrypted, format=None):
 
     A `format` paramater of ``json`` or ``2`` renders JSON output, any
     other value renders XML.
+
     """
     return _render_json(data, encrypted)
+
 
 def _render_json(data, encrypted=False):
     data_json = json.dumps(data)
@@ -567,4 +570,5 @@ def _render_json(data, encrypted=False):
 class GeckoboardException(Exception):
     """
     Represents an error with the Geckoboard decorators.
+
     """
